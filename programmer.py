76ccc748bdb41fe9154dfb7fea2f14f698a5a03f
AmanGullCodes <amangull0049@gmail.com>
2025-09-24 23:58:49 +0500

    Add files via upload

---
+++ programmer.py
@@ -0,0 +1,15 @@
+class Programmar:
+    company = "Microsoft"
+    def __init__(self, name, salary, pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin 
+
+
+
+p = Programmar("Harry", 12000, 102)
+print(p.name, p.salary, p.pin, p.company)
+
+p = Programmar("mohid", 10000, 9)
+print(p.name, p.salary, p.pin, p.company)
+
